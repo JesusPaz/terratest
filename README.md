729ef377a535bb32da5cc2a84ae59549a1bc581d
jpazb <jpazb@psl.com.co>
2020-05-30 16:07:28 -0500

    Add terratest files

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-# terratest
+# Terratest
 Explore Terratest by Gruntwork
 
 ### Run terraform
 
 ```
+    cd terraform
     terraform init
     terraform plan -var-file="varfile.tfvars"
     terraform apply -var-file="varfile.tfvars"
     terraform destroy -var-file="varfile.tfvars"
+
+```
+
+### Run terratest
+
+```go
+
+ cd test
+ go mod init example.com/test
+ // To test you could user
+ go test -v -timeout 30m
+ // or the name of the file
+ go test terraform_basic_example_test.go
+
 ```
 
---
+++ test/terraform_basic_example_test.go
@@ -0,0 +1,46 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// An example of how to test the simple Terraform module in examples/terraform-basic-example using Terratest.
+func TestTerraformBasicExample(t *testing.T) {
+	t.Parallel()
+
+	expectedImage := "ubuntu-18-04-x64"
+	expectedName := "web-1"
+
+	terraformOptions := &terraform.Options{
+		// website::tag::1::Set the path to the Terraform code that will be tested.
+		// The path to where our Terraform code is located
+		TerraformDir: "../terraform/",
+
+		// Variables to pass to our Terraform code using -var-file options
+		VarFiles: []string{"varfile.tfvars"},
+
+		// Disable colors in Terraform commands so its easier to parse stdout/stderr
+		NoColor: true,
+	}
+
+	// website::tag::4::Clean up resources with "terraform destroy". Using "defer" runs the command at the end of the test, whether the test succeeds or fails.
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// website::tag::2::Run "terraform init" and "terraform apply".
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables
+	actualImage := terraform.Output(t, terraformOptions, "image")
+	actualName := terraform.Output(t, terraformOptions, "name")
+
+	// website::tag::3::Check the output against expected values.
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, expectedImage, actualImage)
+	assert.Equal(t, expectedName, actualName)
+
+}
